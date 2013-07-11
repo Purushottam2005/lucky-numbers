f58763eec75a506ef3e2913a397a2236ba10bf0e
etolbakov <Eugene.Tolbakov@dataart.com>
2013-07-11 19:38:27 +0400

    Refactor list retrival with <T> -method()

--- ejbModule/com/management/user/UserManagementServiceBean.java
+++ ejbModule/com/management/user/UserManagementServiceBean.java
@@ -13,15 +13,16 @@ import javax.persistence.TypedQuery;
 import com.ibytecode.entities.Feed;
 import com.ibytecode.entities.Item;
 import com.ibytecode.entities.User;
+// example string
+//String email_query ="SELECT u FROM user u WHERE u.email = :email" ;
 
 @Stateless
 public class UserManagementServiceBean implements UserManagementService, Serializable{
-	// example string
-	String email_query ="SELECT u FROM user u WHERE u.email = :email" ;
 	
-	private static final String USER_BY_EMAIL_QUERY = "SELECT u FROM user u WHERE u.email = ?";
-
 	private static final long serialVersionUID = -7947779666246162567L;
+	private static final String USER_BY_EMAIL_QUERY = "SELECT u FROM user u WHERE u.email = ?";
+	private static final String FEED_LIST_OF_USER_QUERY = "Select f from feed f join f.userList l where l.id = :id";
+	private static final String ITEM_LIST_OF_USER_QUERY = "Select i from item i join i.feed f join f.userList l where l.id = :id";
 
 	//@Resource 
 	@PersistenceContext(unitName = "JPADB")
@@ -29,7 +30,7 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 	
 	@Override
 	public void register(String email, String password, String firstName, String lastName) {
-		System.out.println("inside UserManagementServiceBean,REGISTER,  email: "  + email + " password: " + password + " firstName : " + firstName + " lastName: "+ lastName);		
+		System.out.println("inside UserManagementServiceBean, REGISTER,  email: "  + email + " password: " + password + " firstName : " + firstName + " lastName: "+ lastName);		
 		entityManager.persist(new User(email, password, firstName, lastName)); 
 		entityManager.flush();
 		System.out.println("Insert is completed...");
@@ -47,9 +48,7 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 	@Override
 	public User login(String email, String password) {
 		System.out.println("inside UserManagementServiceBean, LOGIN, email: " + email + " ,password: " + password);
-		///User user = fetchUserByEmail(email);
 		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
-		
 		return user == null || isPasswordWrong(user, password) ? null : user;
 	}
 	
@@ -81,62 +80,42 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 		//TODO implement the method, admin usage
 		return userList;
 	}
+
+
 	
-	
-	//TODO 
-	//javax.persistence.NoResultException: No entity found for query
+	//FIXME This piece of code is repeated with login
 	
 	//TODO refactor getAllItemNewsForUser, getAllFeedsForUser with generics 
+
+//	public List<Item> getAllItemNewsForUser(String email) {
+//		System.out.println("Inside getAllItemNewsForUser, userEmail : " + email);
+//		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
+//		TypedQuery<Item> queryJoinTables = entityManager.createQuery(ITEM_LIST_OF_USER_QUERY, Item.class);
+//		List<Item> itemList = queryJoinTables.setParameter("id", user.getId()).getResultList();
+//		return itemList;
+//	} 
+//
+//	public List<Feed> getAllFeedsForUser(String email) {
+//		System.out.println("Inside getAllFeedsForUser, userEmail : " + email);
+//		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
+//		TypedQuery<Feed> queryJoinTables = entityManager.createQuery(FEED_LIST_OF_USER_QUERY, Feed.class);
+//		List<Feed> feedList = queryJoinTables.setParameter("id", user.getId()).getResultList();
+//		return feedList;
+//	}
+
 	
 	public List<Item> getAllItemNewsForUser(String email) {
-	//public Item getAllItemNewsForUser(String email) {
-		System.out.println("Inside getAllItemNewsForUser, userEmail : " + email);
-
-		//User user = fetchUserByEmail(email);
-		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
-		//TypedQuery<Item> queryJoinTables = entityManager.createQuery("Select i from item i join i.feed f join f.userList l where l.id = :id", Item.class);
-		//List<Item> itemList = queryJoinTables.setParameter("id", user.getId()).getResultList();
-		
-		TypedQuery<Item> queryJoinTables = entityManager.createQuery("Select i from item i", Item.class);
-		List<Item> itemList = queryJoinTables.getResultList();
-		
-		for (Item item : itemList) {
-			System.out.println(item);	
-		}
-		//return itemList.get(0);
-		return itemList;
+		return getListOfInterst(fetchUserByParameter(email, USER_BY_EMAIL_QUERY).getId(), ITEM_LIST_OF_USER_QUERY, Item.class);
 	} 
 
-	//FIXME This piece of code is repeated with login
 	public List<Feed> getAllFeedsForUser(String email) {
-	//public Feed getAllFeedsForUser(String email) {
-	//public List<String> getAllFeedsForUser(String email) {
-		System.out.println("Inside getAllFeedsForUser, userEmail : " + email);
+		return getListOfInterst(fetchUserByParameter(email, USER_BY_EMAIL_QUERY).getId(), FEED_LIST_OF_USER_QUERY, Feed.class);
+	}	
 
-		//User user = fetchUserByEmail(email);
-		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
-		TypedQuery<Feed> queryJoinTables = entityManager.createQuery("Select f from feed f join f.userList l where l.id = :id", Feed.class);
-
-		List<Feed> feedList = queryJoinTables.setParameter("id", user.getId()).getResultList();
-		List<String> feedStringList = new ArrayList<String>();
-		for (Feed feed : feedList) {
-			System.out.println(feed);
-			feedStringList.add(feed.toString());
-		}
-		
-		
-		//return feedList.get(0);
-		return feedList;
-		//return feedStringList;
+	private <T> List<T> getListOfInterst(long id, String query, Class<T> arg)  {
+		return entityManager.createQuery(query, arg).setParameter("id", id).getResultList();
 	}
 
-	
-//	private User fetchUserByEmail(String email) {
-//		//return entityManager.createQuery("SELECT u FROM user u WHERE u.email = :email", User.class).setParameter("email", email).getSingleResult();
-//		return entityManager.createQuery(USER_BY_EMAIL_QUERY, User.class).setParameter(1, email).getSingleResult();
-//	} 
-//	
-	
 	private User fetchUserByParameter(String parameter, String query) {
 		try{
 	        return entityManager.createQuery(query, User.class).setParameter(1, parameter).getSingleResult();
@@ -145,32 +124,10 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 	    	return null;
 	    }
 	} 
-	
-	
-	
-	
-	
-	/*
-	 * 	public BlogEntry saveBlogEntry(BlogEntry blogEntry) {
-		em.persist(blogEntry);
-		return blogEntry;
-	}
- 
-	public List<BlogEntry> findBlogEntries() {
-		final Query query = em.createQuery("SELECT b FROM BlogEntry b ORDER BY b.created DESC");
-		List<BlogEntry> entries = query.getResultList();
-		if (entries == null) {
-			entries = new ArrayList<BlogEntry>();
-		}
-		return entries;
-	}
- 
-	public void deleteBlogEntry(BlogEntry blogEntry) {
-		blogEntry = em.merge(blogEntry);
-		em.remove(blogEntry);
-	}
-	
-	 */
-	
-	
 }
+
+//	private User fetchUserByEmail(String email) {
+//		//return entityManager.createQuery("SELECT u FROM user u WHERE u.email = :email", User.class).setParameter("email", email).getSingleResult();
+//		return entityManager.createQuery(USER_BY_EMAIL_QUERY, User.class).setParameter(1, email).getSingleResult();
+//	} 
+//	
