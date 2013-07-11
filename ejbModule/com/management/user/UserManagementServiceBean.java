435372174d8520f38e9c762a3616d19295972175
etolbakov <Eugene.Tolbakov@dataart.com>
2013-07-11 17:24:58 +0400

    Add non-existing user processing logic

--- ejbModule/com/management/user/UserManagementServiceBean.java
+++ ejbModule/com/management/user/UserManagementServiceBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
@@ -15,6 +16,10 @@ import com.ibytecode.entities.User;
 
 @Stateless
 public class UserManagementServiceBean implements UserManagementService, Serializable{
+	// example string
+	String email_query ="SELECT u FROM user u WHERE u.email = :email" ;
+	
+	private static final String USER_BY_EMAIL_QUERY = "SELECT u FROM user u WHERE u.email = ?";
 
 	private static final long serialVersionUID = -7947779666246162567L;
 
@@ -42,7 +47,9 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 	@Override
 	public User login(String email, String password) {
 		System.out.println("inside UserManagementServiceBean, LOGIN, email: " + email + " ,password: " + password);
-		User user = fetchUserByEmail(email);
+		///User user = fetchUserByEmail(email);
+		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
+		
 		return user == null || isPasswordWrong(user, password) ? null : user;
 	}
 	
@@ -52,11 +59,12 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 	}
 
 
-	public boolean isSuchUserRegistered(String email, String password) {
-		System.out.println("inside UserManagementServiceBean, LOGIN, email: "  + email + " ,password: " + password);
-		User user = fetchUserByEmail(email);
+	public boolean isUserRegisteredAlready(String email) {
+		System.out.println("isUserRegisteredAlready, email: " + email);
+//		User user = fetchUserByEmail(email);
+		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
 		System.out.println("user :" + user);
-		return user != null && password.equals(user.getPassword());
+		return user != null;
 	}
 	
 	
@@ -84,8 +92,8 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 	//public Item getAllItemNewsForUser(String email) {
 		System.out.println("Inside getAllItemNewsForUser, userEmail : " + email);
 
-		User user = fetchUserByEmail(email);
-		
+		//User user = fetchUserByEmail(email);
+		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
 		//TypedQuery<Item> queryJoinTables = entityManager.createQuery("Select i from item i join i.feed f join f.userList l where l.id = :id", Item.class);
 		//List<Item> itemList = queryJoinTables.setParameter("id", user.getId()).getResultList();
 		
@@ -105,8 +113,8 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 	//public List<String> getAllFeedsForUser(String email) {
 		System.out.println("Inside getAllFeedsForUser, userEmail : " + email);
 
-		User user = fetchUserByEmail(email);
-		
+		//User user = fetchUserByEmail(email);
+		User user = fetchUserByParameter(email, USER_BY_EMAIL_QUERY);
 		TypedQuery<Feed> queryJoinTables = entityManager.createQuery("Select f from feed f join f.userList l where l.id = :id", Feed.class);
 
 		List<Feed> feedList = queryJoinTables.setParameter("id", user.getId()).getResultList();
@@ -122,17 +130,20 @@ public class UserManagementServiceBean implements UserManagementService, Seriali
 		//return feedStringList;
 	}
 
-
-	private User fetchUserByEmail(String email) {
-		//return entityManager.createQuery("SELECT u FROM user u WHERE u.email = :email", User.class).setParameter("email", email).getSingleResult();
-		return entityManager.createQuery("SELECT u FROM user u WHERE u.email = ?", User.class).setParameter(1, email).getSingleResult();
-	} 
 	
+//	private User fetchUserByEmail(String email) {
+//		//return entityManager.createQuery("SELECT u FROM user u WHERE u.email = :email", User.class).setParameter("email", email).getSingleResult();
+//		return entityManager.createQuery(USER_BY_EMAIL_QUERY, User.class).setParameter(1, email).getSingleResult();
+//	} 
+//	
 	
 	private User fetchUserByParameter(String parameter, String query) {
-		String email_query ="SELECT u FROM user u WHERE u.email = :email" ;
-		String name_query ="SELECT u FROM user u WHERE u.name = :name" ;
-		return entityManager.createQuery(query, User.class).setParameter(1, parameter).getSingleResult();
+		try{
+	        return entityManager.createQuery(query, User.class).setParameter(1, parameter).getSingleResult();
+	    } catch(NoResultException e) {
+	    	System.err.println("No user found with parameter: " + parameter + " and query: " + query + ", message: " + e);
+	    	return null;
+	    }
 	} 
 	
 	
